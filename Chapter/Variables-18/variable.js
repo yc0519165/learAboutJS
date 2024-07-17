3ef797da7321e4987f5ac43506d60ea1a675bbb2
yc0519165 <yc0519165@gmail.com>
2024-07-17 16:35:40 +0530

    savung

---
+++ Chapter/Variables-18/variable.js
@@ -0,0 +1,21 @@
+
+// let price = Math.round(((500000+2600+12000) + 18/100))
+// let car = `this is the most popular car in the world ${price}`
+// console.log(car)
+
+// let noOfCars = 25;
+// noOfCars = noOfCars * 2;
+// noOfCars = noOfCars *2
+// noOfCars = noOfCars *2
+
+// let money = 5
+
+// money += 2;
+// money -= 1;
+// money *= 2;
+// money /=2;
+// money ++
+
+
+// console.log(money)
+
